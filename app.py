a76087d5251e3c0e22caddf9036fe7bed0200135
Sai Srinivas <gsaimech@gmail.com>
2024-07-12 23:06:22 +0530

    Added DB Integration changes

--- app.py
+++ app.py
@@ -16,9 +16,30 @@ def init_db():
 
 init_db()
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        age = request.form['age']
+        date_of_birth = request.form['date_of_birth']
+
+        conn = get_db_connection()
+        c = conn.cursor()
+        c.execute('Insert into entries (name, email, age, date_of_birth) values (?, ?, ?, ?)', (name, email, int(age), date_of_birth))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('entries'))
+
     return render_template('index.html')
+@app.route('/entries')
+def entries():
+    conn = get_db_connection()
+    c=conn.cursor()
+    cursor = c.execute('Select * from entries')
+    entries = cursor.fetchall()
+    conn.close()
+    return render_template('entries.html', entries=entries)
 
 if __name__ == '__main__':
     app.run(debug=True)
