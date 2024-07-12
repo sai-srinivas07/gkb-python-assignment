bce44bd2f75e300226e084876feba5878303453c
Sai Srinivas <gsaimech@gmail.com>
2024-07-12 22:22:11 +0530

    Added db schema and db creation code

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ import sqlite3
 
 app = Flask(__name__)
 
+def get_db_connection():
+    conn=sqlite3.connect('database.db')
+    return conn
+
+def init_db():
+    conn=get_db_connection()
+    with app.open_resource('schema.sql', mode='r') as f:
+        conn.cursor().executescript(f.read())
+    conn.commit()
+    conn.close()
+
+init_db()
+
 @app.route('/')
 def index():
     return render_template('index.html')
---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS entries (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    age INTEGER NOT NULL,
+    date_of_birth TEXT NOT NULL
+)
